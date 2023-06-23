de782055353dcfebdfb8cec1b260f53b74ff9ee7
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-06-24 09:21:13 +1000

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
     console.log(interaction.data.name)
     if(interaction.data.name == 'yo'){
 	    let leo = await notes.set(interaction.member.user.id.toString(), {
-type: interaction.member.user.global_name
+type: interaction.member.user.id
 })
 
 // get an item at key "leo" from collection animals
