14413cdf77c74b90b549e4a55c885826ce3d1750
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-07-11 05:23:32 +0000

    10210

--- pastelgate.js
+++ pastelgate.js
@@ -1,10 +1,12 @@
 const key = process.env.ENCRYPTION_KEY
+const auth = require('basic-auth')
 module.exports.isAuth = (req, res, next) => {
-    const auth = req.headers.authorization;
-    if (auth === key) {
+    const user = auth(req)
+    if (user['name'] == "pastelgate" && user['pass'] == key) {
       next();
     } else {
       res.status(401);
+      res.setHeader('WWW-Authenticate', 'Basic realm="Node"');
       res.send('Access forbidden');
     }
 }
