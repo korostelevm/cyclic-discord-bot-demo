68bc7fe3f6094e284ed3ea6933201ff2bf35edb5
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-07-12 07:36:57 +0000

    e

--- encryption.js
+++ encryption.js
@@ -2,12 +2,12 @@ const crypto = require('crypto')
 const { ENCRYPTION_TYPE, ENCRYPTION_KEY, ENCRYPTION_IV } = process.env
 const key = crypto
   .createHash('sha512')
-  .update(ENCRYPTION_KEY)
+  .update(Buffer.from(ENCRYPTION_KEY))
   .digest('hex')
   .substring(0, 32)
 const encryptionIV = crypto
   .createHash('sha512')
-  .update(ENCRYPTION_IV)
+  .update(Buffer.from(ENCRYPTION_IV))
   .digest('hex')
   .substring(0, 16)
 
@@ -16,15 +16,15 @@ module.exports.encryptData = function(data) {
   const cipher = crypto.createCipheriv(ENCRYPTION_TYPE, key, encryptionIV)
   return Buffer.from(
     cipher.update(data, 'utf8', 'hex') + cipher.final('hex')
-  ).toString('base64') // Encrypts data and converts to hex and base64
+  ).toString('base64')
 }
 
 // Decrypt data
 module.exports.decryptData = function(encryptedData) {
-  const buff = Buffer.from(encryptedData, 'base64').toString('hex')
+  const buff = Buffer.from(encryptedData, 'base64')
   const decipher = crypto.createDecipheriv(ENCRYPTION_TYPE, key, encryptionIV)
   return (
     decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
     decipher.final('utf8')
-  ) // Decrypts data and converts to utf-8
+  ) 
 }
