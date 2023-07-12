fab6d6bfad8f6b884febef0e9df08d94557d0092
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-07-12 11:25:15 +0000

    node:crypto

--- encryption.js
+++ encryption.js
@@ -1,30 +1,21 @@
-const crypto = require('crypto')
+const crypto = require('node:crypto')
 const { ENCRYPTION_TYPE, ENCRYPTION_KEY, ENCRYPTION_IV } = process.env
-const key = crypto
-  .createHash('sha512')
-  .update(Buffer.from(ENCRYPTION_KEY))
-  .digest('hex')
-  .substring(0, 32)
-const encryptionIV = crypto
-  .createHash('sha512')
-  .update(Buffer.from(ENCRYPTION_IV))
-  .digest('hex')
-  .substring(0, 16)
+const hashKey = crypto.createHash('sha256')
+hashKey.update(ENCRYPTION_KEY)
+const key = hashKey.digest() // .digest('hex').substring(0, 32) // Fix 2: no hex encoding
 
-// Encrypt data
-module.exports.encryptData = function(data) {
-  const cipher = crypto.createCipheriv(ENCRYPTION_TYPE, key, encryptionIV)
+module.exports.encrypt = (encrypted) =>{
+  const cipher = crypto.createDecipheriv('aes-256-cbc', key, ENCRYPTION_IV)
+   cipher.setAutoPadding(false) // Fix 3: disable default PKCS#7 padding
   return Buffer.from(
-    cipher.update(data, 'utf8', 'hex') + cipher.final('hex')
-  ).toString('base64')
+    cipher.update(encrypted, 'utf8', 'hex') + cipher.final('utf8')
+  ).toString("base64")
 }
-
-// Decrypt data
-module.exports.decryptData = function(encryptedData) {
-  const buff = Buffer.from(encryptedData, 'base64')
-  const decipher = crypto.createDecipheriv(ENCRYPTION_TYPE, key, encryptionIV)
-  return (
-    decipher.update(buff.toString('utf8'), 'hex', 'utf8') +
-    decipher.final('utf8')
-  ) 
+module.exports.decrypt = (encrypted) => {
+  let data = Buffer.from(encrypted, "base64").toString()
+  let aes = Buffer.from(data, "hex").toString()
+  let decrypted = decipher.update(aes, 'hex', 'utf-8')
+  decrypted += decipher.final('utf-8')
+  return decrypted
 }
+ 
