1c49d3e62b5c66659ade492a9b2bb91fb3714b82
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2024-04-26 20:46:32 +1000

    e

--- api.js
+++ api.js
@@ -12,29 +12,28 @@ module.exports = function(pref, req, res){
             return res.status(500).json({"type": "invalid preference", "misc": element.name})
         }
       });
-      switch (currentprefs.color){
-      case 'blue':
-        currentprefs.color = 0x00FFFF
+      
+        if (currentprefs.color == "blue"){
+          currentprefs.color = 0x00FFFF
         database.settings(pref, guild, currentprefs)
         return res.status(200).json({"type":"success"}).json({"type":"success"})
-      case 'red':
-        currentprefs.color = 0xFF0000
+        } else if (currentprefs.color == "red"){
+          currentprefs.color = 0xFF0000
         database.settings(pref, guild, currentprefs)
         return res.status(200).json({"type":"success"})
-      case 'green':
-        currentprefs.color = 0x00FF00
+        } else if (currentprefs.color == "green"){
+          currentprefs.color = 0x00FF00
         database.settings(pref, guild, currentprefs)
         return res.status(200).json({"type":"success"})
-      case 'purple':
-        currentprefs.color = 0x800080
+        } else if (currentprefs.color == "purple"){
+          currentprefs.color = 0x800080
         database.settings(pref, guild, currentprefs)
         return res.status(200).json({"type":"success"})
-      default: 
-        currentprefs.color = 0x00FFFF
+        } else {
+          currentprefs.color = 0x00FFFF
         database.settings(pref, guild, currentprefs)
       return res.status(200).json({"type":"success"})
-      }
-      
+        }
     }
    }catch(e){
     console.error(e)
